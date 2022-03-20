584797e0668d0bb807d73f9561dac4d8ed6ee74e
Joshua Simon Thomas <joshua_jst95@hotmail.com>
2022-03-20 10:55:56 +0800

    Add new GET api for obtaining specific envelopes and include budget calculation

--- apiEnvelopes.js
+++ apiEnvelopes.js
@@ -9,10 +9,27 @@ envelopesRouter.get("/", (req, res, next) => {
 envelopesRouter.post("/", (req, res, next) => {
     if(req.is('json')) {
         db.addEnvelope(req.body);
+        console.log(`Total budget is ${db.getTotalBudget()}`)
         res.status(201).send();
     } else {
         res.status(400).send("Envelop data is not in JSON format!");
     }
 });
+
+envelopesRouter.param('envelopeId', (req, res, next, id) => {
+    //tbd
+    next()
+});
+
+envelopesRouter.get("/:envelopeId", (req, res, next) => {
+    const getEnvelope = db.getEnvelopeById(req.params.envelopeId);
+    if(getEnvelope) {
+        res.send(getEnvelope);
+    } else {
+        res.status(404).send();
+    }
+})
+
+
 
 module.exports = envelopesRouter;
--- db.js
+++ db.js
@@ -1,21 +1,43 @@
 let envelopes = [];
+let totalbudget = 0;
 
 const addEnvelope = (newInstance) => {
     //consider adding validation method...
     envelopes.push(newInstance);
+    calculateBudget();
+};
+
+const calculateBudget = () => {
+    envelopes.forEach(env => {
+        totalbudget += env.cost;
+    })
 };
 
 const getAllEnvelopes = () => {
     return envelopes;
+};
+
+const getEnvelopeById = (id) => {
+    for(i=0; i < envelopes.length; i++) {
+        let envelope = envelopes[i];
+        if(envelope.id == id) {
+            return envelope;
+        }
+    }
+    return null;
+};
+
+const getTotalBudget = () => {
+    return totalbudget;
 }
 
-module.exports = { addEnvelope, getAllEnvelopes };
+module.exports = { addEnvelope, getAllEnvelopes, getEnvelopeById, getTotalBudget };
 
 /*
     Envelope JSON structure is as follows,
     {
         id: "1",
         category: "Groceries",
-        cost: "150"
+        cost: 150
     }
 */
